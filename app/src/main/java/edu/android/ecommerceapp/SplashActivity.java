994ed2c702322b5a11ddd3c7c6495c606975336b
imjp19 <jaypatel19124@gmail.com>
2021-04-09 00:19:27 +0530

    pelli commit

---
+++ app/src/main/java/edu/android/ecommerceapp/SplashActivity.java
@@ -0,0 +1,25 @@
+package edu.android.ecommerceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, CustomLoginActivity.class);
+                startActivity(i);
+               // finish();
+            }
+        },3000);
+    }
+
+}
