f3522046926477513a80313bf06cd09d1cbaf0c1
imjp19 <jaypatel19124@gmail.com>
2021-04-09 00:30:33 +0530

    added bloody back button

--- app/src/main/java/edu/android/ecommerceapp/RegisterActivity.java
+++ app/src/main/java/edu/android/ecommerceapp/RegisterActivity.java
@@ -2,7 +2,11 @@ package edu.android.ecommerceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import edu.android.ecommerceapp.ui.login.LoginActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void back(View view) {
+        Intent i = new Intent(RegisterActivity.this, CustomLoginActivity.class);
+        startActivity(i);
+        finish();
     }
 }
