0789c4d80abb93625992ec383cf220555c4c9784
imjp19 <jaypatel19124@gmail.com>
2021-04-09 14:41:18 +0530

    Majjo kari didho

--- app/src/main/java/edu/android/ecommerceapp/RegisterActivity.java
+++ app/src/main/java/edu/android/ecommerceapp/RegisterActivity.java
@@ -13,6 +13,7 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        super.getSupportActionBar().hide();
         setContentView(R.layout.activity_register);
     }
 
